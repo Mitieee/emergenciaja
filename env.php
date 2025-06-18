0a4e66d3d7c80a7e549edfa8b4d8bf21409d10ca
Your Name <you@example.com>
2025-06-18 13:59:30 -0300

    Adicionando env.php com nova senha e conexão segura

---
+++ env.php
@@ -0,0 +1,7 @@
+<?php
+putenv("GOOGLE_API_KEY=AIzaSyCAYL87be8vkNduKrolC_micH2ADQu0xHI");
+
+putenv("DB_HOST=emergencias.mysql.dbaas.com.br");
+putenv("DB_USER=emergencias");
+putenv("DB_PASS=Emergencias25!"); 
+putenv("DB_NAME=emergencias");
